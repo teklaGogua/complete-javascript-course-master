7bf999f1e4162305a41600c02c413678b5526aec
tekla <tekla.gogua.2006@gmail.com>
2022-06-24 15:48:36 +0400

    Declare variables with different data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,30 @@
+/*
+//Values and Variables assignment
 let myCountry = 'Georgia';
 let myContinent = 'Europe/Asia';
 let myPopulation = 3;
 
 console.log(myCountry, myContinent, myPopulation);
+*/
 
+//Data types
+let jsIsFun = true;
+console.log(jsIsFun); //true
 
+console.log(typeof true); //boolean
+console.log(typeof jsIsFun); //boolean
+console.log(typeof 23); //number
+console.log(typeof 'true'); //string
+
+jsIsFun = 'Yes';
+console.log(jsIsFun); //Yes
+
+let year;
+console.log(year); //undefined
+console.log(typeof year); //undefined
+
+year = 2022;
+console.log(year); //2022
+console.log(typeof year); //Number
+
+console.log(typeof null); //It return object, but null's type is null 
