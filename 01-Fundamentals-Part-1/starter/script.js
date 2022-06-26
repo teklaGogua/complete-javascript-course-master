c24c62c1c7231813ace7fb19a644e80a9c1fa443
tekla <tekla.gogua.2006@gmail.com>
2022-06-26 20:48:36 +0400

    Made the switch statement assignment in script.js file, 01

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,11 @@
+/*
 //Values and Variables assignment
 const myCountry = 'Georgia';
 const myContinent = 'Europe/Asia';
 let myPopulation = 3;
 
 console.log(myCountry, myContinent, myPopulation);
-
+*/
 
 /*
 //Data types
@@ -232,6 +233,7 @@ if (myLanguage === 'English' && myPopulation < 50 && !isIsland) {
 }
 */
 
+/*
 //The switch statement
 const day = 'monday';
 
@@ -271,4 +273,29 @@ if (day === 'monday') {
     console.log('Enjoy the weekend :D');
 } else {
     console.log('Not a valid day!');
+}
+*/
+
+//The switch statement assignment
+const language = myLanguage;
+
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too :D');
 }
