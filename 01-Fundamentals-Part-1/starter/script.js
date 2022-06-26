fa64d4eb7ee548468d6896f59fdc3fd1387109db
tekla <tekla.gogua.2006@gmail.com>
2022-06-26 20:31:01 +0400

    made example of switch statement in script.js file, 01

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -223,10 +223,38 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 };
 */
 
+/*
 //Logical Operators assignment
 if (myLanguage === 'English' && myPopulation < 50 && !isIsland) {
     console.log(`You should live in ${myCountry} :)`);
 } else {
     console.log(`${myCountry} does not meet your criteria :(`);
+}
+*/
+
+//The switch statement
+const day = 'monday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
 }
 
