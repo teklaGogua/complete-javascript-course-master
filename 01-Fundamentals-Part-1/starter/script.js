099a53a5e5b074d971d6728b5fdfcf8eb62bd5e6
tekla <tekla.gogua.2006@gmail.com>
2022-06-26 21:06:04 +0400

    Made the conditional operator examples in script.js file, 01

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -276,6 +276,7 @@ if (day === 'monday') {
 }
 */
 
+/*
 //The switch statement assignment
 const language = myLanguage;
 
@@ -299,3 +300,21 @@ switch (language) {
     default:
         console.log('Great language too :D');
 }
+*/
+
+//The Conditional (Ternary) Operator
+const age = 16;
+age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water'); //Statement
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink); //Expression
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
