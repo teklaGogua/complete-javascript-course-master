68bcc461d158e6c50cad33965093777df44bded3
tekla <tekla.gogua.2006@gmail.com>
2022-06-26 20:37:00 +0400

    Made same aplication in script.js with if/else statement, 01

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -258,3 +258,17 @@ switch (day) {
         console.log('Not a valid day!');
 }
 
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
+}
