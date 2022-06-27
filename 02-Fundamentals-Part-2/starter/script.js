8c7b7deb27064169fd753fbac0ddef097c2eba41
tekla <tekla.gogua.2006@gmail.com>
2022-06-27 15:17:43 +0400

    Wrote besic Functions in script.js, 02

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 //Activating strict moda
 let hasDriversLicense = false;
 const passTest = true;
@@ -10,3 +11,24 @@ if (hasDriversLicense) console.log('I can drive :D');
 //const interface = 'Audio';
 //const private = 534;
 //const if = 23;
+*/
+
+//Functions
+function logger() {
+    console.log("My name is Tekla");
+}
+
+logger(); //Calling / running / invoking functions
+logger();
+logger(); //You can reuse it many times
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
