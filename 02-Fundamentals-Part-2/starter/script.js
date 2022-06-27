10d6a4d68799875bac7db8fcd7cdfc17c8e4ac5c
tekla <tekla.gogua.2006@gmail.com>
2022-06-27 14:59:36 +0400

    Activated strict mode in script.js file, 02

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//Activating strict moda
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+//const interface = 'Audio';
+//const private = 534;
+//const if = 23;
