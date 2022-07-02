10f289c5c24852235d4831213a38d7244d7f7e11
tekla <tekla.gogua.2006@gmail.com>
2022-07-02 19:48:55 +0400

    Added some comments and Manipulated CSS Styles in script.js file, 05

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -20,10 +20,19 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+
+    // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '☝️ Too high!';
@@ -33,6 +42,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '🤕 You lost a game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '👇 Too low!';
