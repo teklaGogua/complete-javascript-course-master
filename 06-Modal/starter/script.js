2f1bcf3638604a848ed80710616881ea7a16ae3e
tekla <tekla.gogua.2006@gmail.com>
2022-07-03 14:04:36 +0400

    Selected elements and looped btnsShowModel in script.js file, 06

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const model = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const btnsShowModel = document.querySelectorAll('.show-modal');
+console.log(btnsShowModel);
+
+for (let i = 0; i < btnsShowModel.length; i++)
+  console.log(btnsShowModel[i].textContent);
