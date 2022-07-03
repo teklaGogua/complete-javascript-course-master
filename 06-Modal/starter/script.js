32a690910aa81b3159cebff47d0e7f159537e603
tekla <tekla.gogua.2006@gmail.com>
2022-07-03 15:02:22 +0400

    Gavefunctionality to app in script.js file, 06

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,10 +1,22 @@
 'use strict';
 
-const model = document.querySelector('.modal');
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btn = document.querySelector('.close-modal');
-const btnsShowModel = document.querySelectorAll('.show-modal');
-console.log(btnsShowModel);
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsShowModel.length; i++)
-  console.log(btnsShowModel[i].textContent);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
