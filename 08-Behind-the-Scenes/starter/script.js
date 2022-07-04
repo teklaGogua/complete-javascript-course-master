ef5da9065901d07492a7d50d50dfa9237a7f269e
tekla <tekla.gogua.2006@gmail.com>
2022-07-04 21:06:48 +0400

    Practised scoping in script.js file, 0,8

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function clacAge(birthYear) {
+  const age = 2022 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear > 1996) {
+      var millenial = false;
+      // Creating New variable with same name as outer scope's variable
+      const firstName = 'Steven';
+
+      // Reassigning outer scope's variable
+      output = 'NEW OUTPUT!';
+
+      const str = `Oh, and you're a gen-z, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Tekla';
+clacAge(2006);
+// console.log(age);
+// printAge();
