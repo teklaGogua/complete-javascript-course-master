2592da034eebd7884b2188057460d5947ca7d0b7
tekla <tekla.gogua.2006@gmail.com>
2022-07-05 23:24:08 +0400

    Saw how Hoisting and TDZ work in Practise in script.js file, 08

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//////////////////////////////////////
+// Scoping in Practice
+/*
 function clacAge(birthYear) {
   const age = 2022 - birthYear;
 
@@ -36,3 +39,50 @@ const firstName = 'Tekla';
 clacAge(2006);
 // console.log(age);
 // printAge();
+*/
+
+//////////////////////////////////////
+// Hoisying and TDZ in Practise
+
+// Variables
+console.log(me); // Undefined
+// console.log(job); // Uncaught ReferenceError: Cannot access 'job' before initialization
+// console.log(year); // Uncaught ReferenceError: Cannot access 'year' before initialization
+
+var me = 'Tekla';
+let job = 'programmer';
+const year = 2006;
+
+// Functions
+console.log(addDecl(2, 3)); // 5
+// console.log(addExpr(2, 3)); // Uncaught ReferenceError: Cannot access 'addExpr' before initialization
+console.log(addArrow); // undefined
+// console.log(addArrow(2, 3)); // Uncaught TypeError: addArrow is not a function
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) delateShoppingCart();
+
+var numProducts = 10;
+
+function delateShoppingCart() {
+  console.log('All products delated!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); // true
+console.log(y === window.y); // false
+console.log(z === window.z); // false
